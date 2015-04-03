ce366bffcb154b11c6f02d6642adc9d54050d8ed
Paul Fitzgerald <paulfitz99@gmail.com>
2015-04-03 13:55:44 +0100

    8 array tests passing

--- app/arrays.js
+++ app/arrays.js
@@ -43,15 +43,18 @@ define(function() {
     },
 
     prepend : function(arr, item) {
-
+        arr.unshift(item);
+        return arr;
     },
 
     curtail : function(arr) {
-
+        arr.shift();
+        return arr;
     },
 
     concat : function(arr1, arr2) {
-
+        var result = arr1.concat(arr2);
+        return result;
     },
 
     insert : function(arr, item, index) {
