43dfbbf19ea18b6b8f1d2a25ff6588d3183cb8d1
Paul Fitzgerald <p.fitzgerald@getyourhero.com>
2015-12-29 21:43:55 +0100

    Remove global variable

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,11 +8,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     globals : function() {
-      var myObject = {
-        name : 'Jory'
-      };
-
-      return myObject;
+      
     },
 
     functions : function(flag) {
